12c7d4bdb965014ac81bef5dbbe23538751908e6
philip.pannenko <philip.pannenko@dealertrack.com>
2014-12-17 14:15:42 -0500

    updated directive

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -5,20 +5,48 @@ myApp.factory('UsersResource', function ($resource) {
   return $resource('/api/users/:id');
 });
 
-myApp.controller('AppCtrl', function($scope, $http, UsersResource, $q) {
+myApp.controller('AppCtrl', function($scope, $http, UsersResource, $q, $log) {
   
   $scope.users = [];
+  $scope.selectedUser;
   
   $scope.getUsers = function() {
     UsersResource.get().$promise.then(function(result) {
-      debugger;
       $scope.users = result.payload;
     }, function (error) {
-      debugger;
       error.type = "danger";
-      $scope.$broadcast("myEvent", error);
+      $log.log(user + " saved");
     });
   }
   
+  $scope.editUser = function(user) {
+    $scope.selectedUser = user;
+  }
+  
+  $scope.saveUser = function(user) {
+    debugger;
+    $log.log(user + " saved");
+    $scope.selectedUser = null;
+  }
+  
   $scope.getUsers();
+});
+
+myApp.directive('dwngrokuInput',function($compile) {
+  
+  return {
+    restrict: "A",
+    require: "^form",
+    replace: true,
+    scope: {
+      myDirVar : '='    
+    },
+    templateUrl: 'templateUrl/dwngrokuInput.html',
+    compile: function(tElem, tAttrs){
+      var html = tElem.html().replace(/attrs.name/g, tAttrs.name);
+      html = html.replace(/attrs.propLabelName/g, tAttrs.propLabelName);
+      html = html.replace(/attrs.propModel/g, tAttrs.propModel);
+      tElem.html(html);
+    }
+  }
 });
