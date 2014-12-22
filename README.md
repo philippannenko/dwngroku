31476fea6da1424d72bc963f2b9c668f7f1f2dbd
philip.pannenko <philip.pannenko@dealertrack.com>
2014-12-22 13:24:16 -0500

    added login/logout. added todo task list

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Run by adding to args:
 	server example.yml 
 
 with the following as a env var IS_LOCAL
+
+
+++ tasks
+* add preloading of data in router
+* follow this example to do ng auth: http://angular-client-side-auth.herokuapp.com/login/
+* general cleanup of how methods are called and etc.
--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -1,24 +1,104 @@
 var myApp = angular.module('myApp', ['ngResource','ui.bootstrap','angular-loading-bar', 'ngAnimate','duScroll','ngRoute']);
 
-myApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/user-list.html',
-        controller: 'UserListCtrl'
-      }).when('/:id', {
-        templateUrl: 'partials/user-details.html',
-        controller: 'UserDetailsCtrl'
-      }).otherwise({
-        redirectTo: '/'
-      });
-  }]);
+myApp.config(['$routeProvider', 'USER_ROLES', function($routeProvider, USER_ROLES) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'partials/user-list.html',
+      controller: 'UserListCtrl',
+      authorizedRoles: [USER_ROLES.admin, USER_ROLES.editor]
+    }).when('/user/:id', {
+      templateUrl: 'partials/user-details.html',
+      controller: 'UserDetailsCtrl',
+      authorizedRoles: [USER_ROLES.admin, USER_ROLES.editor]
+    }).when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'LoginController'
+    }).otherwise({
+      redirectTo: '/'
+    });
+}]);
+
+myApp.constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+})
+
+myApp.constant('USER_ROLES', {
+  all: '*',
+  admin: 'admin',
+  editor: 'editor',
+  guest: 'guest'
+})
+
+// ////////////// FACTORIES ///////////// //
 
 myApp.factory('UsersResource', function ($resource) {
   return $resource('/api/users/:id');
 });
 
-myApp.controller('AppCtrl', function($scope, $log){
+myApp.factory('AuthService', function ($http, Session, $q) {
+  var authService = {};
+  
+  authService.logout = function () {
+    Session.destroy();
+  }
+  
+  authService.getAuthUser = function () {
+    return {id: Session.userId, name:Session.userName, userRole: Session.userRole};
+  }
+ 
+  authService.login = function (credentials) {
+    
+    var deferred = $q.defer();
+    
+    setTimeout(function() {
+      Session.create(1, 'name','admin');
+      deferred.resolve({id:1,name:'name',userRole:'admin'});
+    }, 1000);
+
+    return deferred.promise;
+  };
+ 
+  authService.isAuthenticated = function () {
+    return !!Session.userId;
+  };
+ 
+  authService.isAuthorized = function (authorizedRoles) {
+    if (!angular.isArray(authorizedRoles)) {
+      authorizedRoles = [authorizedRoles];
+    }
+    return (authService.isAuthenticated() &&
+      authorizedRoles.indexOf(Session.userRole) !== -1);
+  };
+ 
+  return authService;
+})
+
+// ////////////// SERVICES ///////////// //
+
+myApp.service('Session', function () {
+  this.create = function (userId, userName, userRole) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userRole = userRole;
+  };
+  this.destroy = function () {
+    this.userId = null;
+    this.userName = null;
+    this.userRole = null;
+  };
+  return this;
+})
+
+
+
+// ////////////// CONTROLERS ///////////// //
+
+myApp.controller('AppCtrl', function($scope, AUTH_EVENTS, $rootScope, $log, USER_ROLES, AuthService, $location){
   
   $scope.alerts = [];
   $scope.alertsBetweenPages = [];
@@ -64,6 +144,38 @@ myApp.controller('AppCtrl', function($scope, $log){
     $scope.alerts = [];
   });
   
+  $scope.currentUser = AuthService.getAuthUser();
+ 
+  $scope.setCurrentUser = function (user) {
+    $scope.currentUser = user;
+  };
+  
+  $scope.$on(AUTH_EVENTS.notAuthenticated, function() {$location.path("\login")});
+  $scope.$on(AUTH_EVENTS.sessionTimeout, function() {$location.path("\login")});
+  $scope.$on(AUTH_EVENTS.loginSuccess, function() {$location.path('/')});
+  
+  $scope.$on('$routeChangeStart', function routeAuthorized(scope, next) {
+    var route = next || $route.current;
+    var authorizedRoles = route.authorizedRoles;
+    if (!AuthService.isAuthorized(authorizedRoles)) {
+      event.preventDefault();
+      if (AuthService.isAuthenticated()) {
+        // user is not allowed
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+      } else {
+        // user is not logged in
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+      }
+    }
+  });
+  
+  $scope.logout = function() {
+    AuthService.logout();
+    $scope.setCurrentUser(null);
+    $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+    $scope.$emit('addMessageBetweenPages', {type:'info',message:'Logout success'});
+  }
+
 })
 
 myApp.controller('UserDetailsCtrl', function($scope, $http, UsersResource, $q, cfpLoadingBar, $location, $document, $routeParams) {
@@ -159,6 +271,27 @@ myApp.controller('UserListCtrl', function($scope, $http, UsersResource, $q, cfpL
   
   $scope.getUsers();
 });
+
+myApp.controller('LoginController', function ($location, $scope, $rootScope, AUTH_EVENTS, AuthService) {
+  $scope.$emit('newPageLoaded');
+  
+  $scope.credentials = {
+    username: '',
+    password: ''
+  };
+  $scope.login = function (credentials) {
+    AuthService.login(credentials).then(function (user) {
+      $scope.setCurrentUser(user);
+      $scope.$emit('addMessageBetweenPages', {type:'info',message:'Login success'});
+      $scope.$emit(AUTH_EVENTS.loginSuccess);
+    }, function () {
+      $scope.$emit(AUTH_EVENTS.loginFailed);
+    });
+  };
+  
+})
+
+// ////////////// DIRECTIVES ///////////// //
 
 myApp.directive('dwngrokuInput',function($compile) {
   
